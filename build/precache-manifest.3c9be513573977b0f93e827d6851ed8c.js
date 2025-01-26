18a78aeff2ab80b56972a65f21ffea3af0ce33bf
venkata naga sai kumar cheedella <saicheedella@venkatas-MacBook-Air.local>
2025-01-26 12:04:27 -0600

    production build

---
+++ build/precache-manifest.3c9be513573977b0f93e827d6851ed8c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fa835414caea97bfbefeb7930dc0ad9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b22571dab346c857e3a0",
+    "url": "/static/css/2.1ba42cb2.chunk.css"
+  },
+  {
+    "revision": "ab6d177458a22b071ac3",
+    "url": "/static/css/main.c511ee34.chunk.css"
+  },
+  {
+    "revision": "b22571dab346c857e3a0",
+    "url": "/static/js/2.fc944518.chunk.js"
+  },
+  {
+    "revision": "5f3334bb1f87d5ab82109bb026761b0f",
+    "url": "/static/js/2.fc944518.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab6d177458a22b071ac3",
+    "url": "/static/js/main.98c7f2ae.chunk.js"
+  },
+  {
+    "revision": "73b5a0009db405ff3085",
+    "url": "/static/js/runtime-main.9cd8021f.js"
+  },
+  {
+    "revision": "cc40e615939d66cb56feafc2f365de49",
+    "url": "/static/media/rxicon.cc40e615.png"
+  }
+]);
